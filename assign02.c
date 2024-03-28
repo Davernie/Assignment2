92b7fa04636528c01ae4c8dfdd4ab9632a20d984
AWatters310 <adamwatters310@gmail.com>
2024-03-28 17:11:36 +0000

    Add code for initialisation and interrupt setup

--- assign02.c
+++ assign02.c
@@ -1,5 +1,34 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
 // Must declare the main assembly entry point before use.
 void main_asm();
+
+// Initialise a GPIO pin – see SDK for detail on gpio_init()
+void asm_gpio_init(uint pin) {
+    gpio_init(pin);
+}
+
+// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+void asm_gpio_set_dir(uint pin, bool out) {
+    gpio_set_dir(pin, out);
+}
+
+// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+bool asm_gpio_get(uint pin) {
+    return gpio_get(pin);
+}
+
+// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+void asm_gpio_put(uint pin, bool value) {
+    gpio_put(pin, value);
+}
+
+// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+void asm_gpio_set_irq(uint pin) {
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
+}
 
 /*
  * Main entry point for the code - simply calls the main assembly function.
