8738833c724aa5f30d6451f2799099565663328f
Davernie <127890004+Davernie@users.noreply.github.com>
2024-04-04 11:53:11 +0100

    Delete game-screen.c

--- game-screen.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void displayInfo(struct player p, struct letter l) {
-    printf("Level -  %d\n", p.currentLevel);
-    printf("Lives: %d\n", p.lives);
-    printf("Wins: %d\n" , p.currentWins);
-    printf("\n LETTER[%c] - MORSE CODE[%s]\n" , l.letter, l.morse_Code);
-}
-
